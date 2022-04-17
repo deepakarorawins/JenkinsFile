01422b8e5d626f27ebe0cf0f811c1af5fdfb5c47
deepakarora <deepakarorawins@gmail.com>
2022-04-17 11:31:55 +0530

    Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -2,7 +2,7 @@ import groovy.sql.Sql
 
 def output = []
 
-def sql = Sql.newInstance('jdbc:sqlserver://DeeAutomation.mssql.somee.com:1433;databaseName=DeeAutomation;user=manhattan1902_SQLLogin_1;password=gg2yrvf7hc;encrypt=true;trustServerCertificate=true;')
+def sql = Sql.newInstance('jdbc:sqlserver://DeeAutomation.mssql.somee.com;databaseName=DeeAutomation;user=manhattan1902_SQLLogin_1;password=gg2yrvf7hc;encrypt=true;trustServerCertificate=true;')
 String sqlString = "SELECT ClassName FROM regression;"
 sql.eachRow(sqlString){ row ->
 	output.push(row[0])
