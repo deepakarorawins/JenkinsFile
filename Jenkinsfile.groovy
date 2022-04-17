bd9789f0c6992a72ee0e1dabd9ed27275410ecbf
deepakarora <deepakarorawins@gmail.com>
2022-04-17 10:48:31 +0530

    Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,9 +1,16 @@
-@Grab('com.oracle:ojdbc6:11.2.0.4')
-import groovy.sql.Sql;
-import java.util.ServiceLoader;
-import java.sql.Driver;
+//@Grab(group = 'my.compay', module='my-module-name', version='1.0.0-SNAPSHOT')
+//import my.company.MyFancyClass
+@Grab('com.google.guava:guava:23.0')
+import com.google.common.base.Joiner
 
-ServiceLoader<Driver> loader = ServiceLoader.load(Driver.class);
-def sql = Sql.newInstance("jdbc:sqlserver://DeeAutomation.mssql.somee.com:1433;databaseName=DeeAutomation;user=manhattan1902_SQLLogin_1;password=gg2yrvf7hc;encrypt=true;trustServerCertificate=true;")
-sql.execute 'select 1 from dual'
-sql.close()
+pipeline {
+	agent any
+	stages {
+		stage('Grape Test') {
+			steps {
+				echo "Joiner: ${Joiner.class}"
+				// echo "MyFancyClass: ${MyFancyClass.class}"
+			}
+		}
+	}
+}
